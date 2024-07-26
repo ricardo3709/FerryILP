2fb12e8c3ff91dcba443ec4d814a0979ea5b99fa
LinLin001105 <linlin001105@outlook.com>
2024-07-26 13:58:55 +1000

    F10test

--- ILPimplementation/main.py
+++ ILPimplementation/main.py
@@ -50,18 +50,18 @@ config = SimulationConfig(
 x, y, Q, z, Z, Z_prime = define_variables(model, config, cal_C, cal_Rl, cal_C_lS)
 
 # Flag to decide whether to generate new files
-# generate_new_files = True  
-generate_new_files = False 
+generate_new_files = True  
+# generate_new_files = False 
 
 
 # Prefix for file names
-file_prefix = "6htest_1"  # You can change the prefix as needed
+file_prefix = "6htest_F10"  # You can change the prefix as needed
 
 # Manage results based on the flag
 results = manage_results(config, generate_new_files, file_prefix)
 
 if results:
-    taskF_results, mu_results, xi_results, phi_results, E_results = results
+    taskF_results, mu_results, xi_results, phi_results, E_results, nu_results = results
     print('Results loaded successfully.\n')
 else:
     print('Results generated and saved successfully.\n')
@@ -76,10 +76,12 @@ else:
         phi_results = pickle.load(f)
     with open(f'ILPimplementation/pkl_files/{file_prefix}_E_results.pkl', 'rb') as f:
         E_results = pickle.load(f)
+    with open(f'ILPimplementation/pkl_files/{file_prefix}_nu_results.pkl', 'rb') as f:
+        nu_results = pickle.load(f)
 
 
 # Add constraints
-add_constraints(model, config, x, y, Q, z, Z, Z_prime, phi_results, E_results, mu_results, taskF_results, xi_results)
+add_constraints(model, config, x, y, Q, z, Z, Z_prime, phi_results, E_results, mu_results, taskF_results, xi_results, nu_results)
 
 # Set objective functions
 psi = set_objective_functions(model, config, y, phi_results)
